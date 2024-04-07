9ed33cf08d90245068e930e246146591122919a2
sarrazenkri3 <100694256+sarrazenkri3@users.noreply.github.com>
2024-04-08 00:14:02 +0200

    update front and fix bug

--- front/js/Profile.js
+++ front/js/Profile.js
@@ -12,13 +12,12 @@ function searchUser() {
         .catch(error => console.error('Error searching for users:', error));
 }
 
-
 // Function to display search results
 // Function to handle displaying users in a popup
 function displayUsersPopup(users, addFriendFunction) {
     // Create a div element to hold the popup content
     const popupDiv = document.createElement('div');
-    popupDiv.classList.add('popup');
+    popupDiv.classList.add('messaging_box');
     popupDiv.id = 'userPopup'; 
 
     // Create a div element to display the users
@@ -32,14 +31,22 @@ function displayUsersPopup(users, addFriendFunction) {
     users.forEach(user => {
         const listItem = document.createElement('li');
         const addButton = document.createElement('button');
-        addButton.textContent = 'Add Friend';
+        addButton.textContent = 'Add';
+        addButton.classList.add('btn-small');
+        addButton.style.position = 'relative'; // ou 'absolute' selon votre besoin
+        addButton.style.alignItems ='center';
+        addButton.style.zIndex = '9999'; // Mettre le bouton au premier plan
+        
+
+
         addButton.addEventListener('click', () => {
             // Call the addFriendFunction with the username as an argument
             addFriendFunction(user.username);
         });
         listItem.textContent = user.username;
-        listItem.appendChild(addButton);
         userList.appendChild(listItem);
+        listItem.appendChild(addButton);
+       // userList.appendChild(listItem);
     });
 
     // Append the list to the usersDiv
@@ -48,6 +55,12 @@ function displayUsersPopup(users, addFriendFunction) {
     // Create a "Go Back" button
     const goBackButton = document.createElement('button');
     goBackButton.textContent = 'Go Back';
+    goBackButton.classList.add('btn-close');
+    goBackButton.style.position = 'relative'; // ou 'absolute' selon votre besoin
+    goBackButton.style.zIndex = '9999'; // Mettre le bouton au premier plan
+    goBackButton.style.left = '50%';
+    goBackButton.style.top = '50%';
+
     goBackButton.addEventListener('click', () => {
         document.body.removeChild(popupDiv); // Close the popup when "Go Back" button is clicked
     });
@@ -59,6 +72,78 @@ function displayUsersPopup(users, addFriendFunction) {
     // Append the popupDiv to the body
     document.body.appendChild(popupDiv);
 }
+
+function listFriendRequests() {
+    // Assuming you have an API endpoint to retrieve friend requests
+    fetch(`/api/user/friendRequests`)
+        .then(response => response.json())
+        .then(data => displayFriendRequestsPopup(data))
+        .catch(error => console.error('Error fetching friend requests:', error));
+}
+
+function displayFriendRequestsPopup(requests) {
+    // Create a div element to hold the popup content
+    const popupDiv = document.createElement('div');
+    popupDiv.classList.add('messaging_box');
+    popupDiv.id = 'friendRequestsPopup'; 
+
+    // Create a div element to display the friend requests
+    const requestsDiv = document.createElement('div');
+    requestsDiv.classList.add('user-list');
+
+
+    // Create an unordered list to list the friend requests
+    const requestsList = document.createElement('ul');
+
+    // Add each friend request to the list
+    requests.forEach(request => {
+        const listItem = document.createElement('li');
+        const acceptButton = document.createElement('button');
+        acceptButton.textContent = 'Accept';
+        acceptButton.classList.add('btn-small');
+        acceptButton.style.position = 'relative'; // ou 'absolute' selon votre besoin
+        acceptButton.style.alignItems ='center';
+        acceptButton.style.zIndex = '9999'; // Mettre le bouton au premier plan
+
+        acceptButton.addEventListener('click', () => {
+            // Call the acceptFriendRequest function with the request ID as an argument
+            acceptFriendRequest(request._id);
+        });
+        
+        listItem.textContent = request.username;
+        listItem.appendChild(acceptButton);
+        requestsList.appendChild(listItem);
+    });
+
+    // Append the list to the requestsDiv
+    requestsDiv.appendChild(requestsList);
+
+    // Create a "Close" button
+    const closeButton = document.createElement('button');
+    closeButton.textContent = 'Close';
+    closeButton.classList.add('btn-close');
+    closeButton.style.position = 'relative'; // ou 'absolute' selon votre besoin
+    closeButton.style.zIndex = '9999'; // Mettre le bouton au premier plan
+    closeButton.style.left = '50%';
+    closeButton.style.top = '50%';
+    closeButton.addEventListener('click', () => {
+        document.body.removeChild(popupDiv); // Close the popup when "Close" button is clicked
+    });
+
+   
+
+
+
+
+    // Append the requestsDiv and closeButton to the popupDiv
+    popupDiv.appendChild(requestsDiv);
+    popupDiv.appendChild(closeButton);
+
+    // Append the popupDiv to the body
+    document.body.appendChild(popupDiv);
+}
+
+
 function afficherNotification(message) {
     const notification = document.createElement('div');
     notification.className = 'notification';
@@ -84,6 +169,7 @@ function afficherNotification(message) {
             notification.remove();
         }
     });
+
 }
 
 
@@ -121,58 +207,6 @@ function addFriend(username) {
     document.body.removeChild(popupDiv); // Close the popup when "Go Back" button is clicked
 }
 
-function listFriendRequests() {
-    // Assuming you have an API endpoint to retrieve friend requests
-    fetch(`/api/user/friendRequests`)
-        .then(response => response.json())
-        .then(data => displayFriendRequestsPopup(data))
-        .catch(error => console.error('Error fetching friend requests:', error));
-}
-
-function displayFriendRequestsPopup(requests) {
-    // Create a div element to hold the popup content
-    const popupDiv = document.createElement('div');
-    popupDiv.classList.add('popup');
-    popupDiv.id = 'friendRequestsPopup'; 
-
-    // Create a div element to display the friend requests
-    const requestsDiv = document.createElement('div');
-    requestsDiv.classList.add('friend-requests-list');
-
-    // Create an unordered list to list the friend requests
-    const requestsList = document.createElement('ul');
-
-    // Add each friend request to the list
-    requests.forEach(request => {
-        const listItem = document.createElement('li');
-        const acceptButton = document.createElement('button');
-        acceptButton.textContent = 'Accept';
-        acceptButton.addEventListener('click', () => {
-            // Call the acceptFriendRequest function with the request ID as an argument
-            acceptFriendRequest(request._id);
-        });
-        listItem.textContent = request.username;
-        listItem.appendChild(acceptButton);
-        requestsList.appendChild(listItem);
-    });
-
-    // Append the list to the requestsDiv
-    requestsDiv.appendChild(requestsList);
-
-    // Create a "Close" button
-    const closeButton = document.createElement('button');
-    closeButton.textContent = 'Close';
-    closeButton.addEventListener('click', () => {
-        document.body.removeChild(popupDiv); // Close the popup when "Close" button is clicked
-    });
-
-    // Append the requestsDiv and closeButton to the popupDiv
-    popupDiv.appendChild(requestsDiv);
-    popupDiv.appendChild(closeButton);
-
-    // Append the popupDiv to the body
-    document.body.appendChild(popupDiv);
-}
 
 function acceptFriendRequest(requestId) {
     // Assuming you have an API endpoint to accept friend requests
