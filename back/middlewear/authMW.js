879bc6679ab494e5025eaaaf945131011964ee0c
SkanderMeziou <skander.meziou@etu.unice.fr>
2024-04-22 11:08:24 +0200

    testlogin

--- back/middlewear/authMW.js
+++ back/middlewear/authMW.js
@@ -2,28 +2,34 @@ const jwt = require('jsonwebtoken');
 const parseCookies = require("../config/CookieParser");
 require('dotenv').config(); // Load environment variables from .env file
 
-function authMiddleware(req, res, next) {
-    // Get the token from the request headers
-    let token;
-    if (req.headers.cookie) {
-      const cookies = parseCookies(req.headers.cookie);
-      token = cookies.jwt;
-    } else {
-      res.writeHead(302, {
-        Location: "http://localhost:8000/html/Auth/Login.html",
-      });
-      return res.end();
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
     }
+    return null;
+}
+
+function authMiddleware(req, res, next) {
+    // Get the token from the cookie
+    let token = getCookie('jwt', req);
 
     if (!token) {
         // Redirect to the login page if token is missing
+        const loginUrl = `http://${process.env.SERVER_IP}/html/Auth/Login.html`; // Replace with your actual server IP
 
-        res.writeHead(302, { 'Location': 'http://localhost:8000/html/Auth/Login.html' }); 
+        if (!loginUrl) {
+            throw new Error('Missing SERVER_IP environment variable');
+        }
+
+        res.writeHead(302, { Location: loginUrl });
         return res.end();
     }
 
     try {
-
         // Verify the token using the secret key from .env file
         const decoded = jwt.verify(token, process.env.jwt_secret);
 
@@ -31,12 +37,20 @@ function authMiddleware(req, res, next) {
         req.user = decoded;
 
         // Call the next middleware or route handler
-        next(req,res);
+        next(req, res);
     } catch (error) {
         // Redirect to the login page if token is invalid
-        res.writeHead(302, { 'Location': 'http://localhost:8000/html/Auth/Login.html' }); 
+        const loginUrl = `http://${process.env.SERVER_IP}/html/Auth/Login.html`; // Replace with your actual server IP
+
+        if (!loginUrl) {
+            throw new Error('Missing SERVER_IP environment variable');
+        }
+
+        res.writeHead(302, { Location: loginUrl });
         return res.end();
     }
 }
 
 module.exports = authMiddleware;
+
+
